88c09c0806566cd404c5e22d99a0afb81a1ba4a7
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-10-12 11:35:03 -0300

    simple transaction integration tests

---
+++ src/__tests__/Transaction.spec.js
@@ -0,0 +1,58 @@
+const request = require("supertest");
+const app = require("../app");
+//const connection = require("../database/connection");
+
+//beforeEach(async () => {
+  //await connection.migrate.rollback();
+  //await connection.migrate.latest();
+//});
+
+//afterAll(async () => {
+  //await connection.destroy();
+//});
+
+describe("Create transaction", () => {
+  it('Should be able to create a transactions', async () => {
+    const response = await request(app).post('/transactions').send({
+      transaction: [
+        {
+          type: "income",
+          value: 2230.83,
+          description: "Salary paid to Homer",
+          category: {
+            name: "Salary"
+          }
+        }
+      ]
+    });
+
+    expect(response.body).toMatchObject({ message: 'Transaction successfully completed!' });
+ });
+});
+
+describe("List transactions", () => {
+  it('Should be able to list transactions', async () => {
+    const response = await request(app).get('/transactions');
+
+    expect(response.body).toHaveLength(1);
+ });
+});
+
+describe("List balance values", () => {
+  it('Should be able to show total balance', async () => {
+    const response = await request(app).get('/cashiers');
+
+    expect(response.body.transactions).toHaveLength(1);
+    expect(response.body).toMatchObject({
+      income: 2230.83,
+      outcome: 0,
+      total_balance: 2230.83,
+   });
+ });
+});
+
+
+
+
+
+
