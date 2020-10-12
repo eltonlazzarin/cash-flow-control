dd14bbc9bbf839bb27052fe9c29cb40435166d1d
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-10-12 11:32:29 -0300

    connection file

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,8 @@
 const knex = require('knex');
+const configuration = require('../../knexfile');
 
-const connection = knex({
-  client: 'pg',
-  connection: {
-    database: 'vcaixa_db',
-    user: 'postgres',
-    password: 'docker',
-  },
-});
+const config = process.env.NODE_ENV === 'test' ? configuration.development : configuration.development;
+
+const connection = knex(config);
 
 module.exports = connection;
