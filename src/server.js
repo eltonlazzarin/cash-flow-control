49b526db990f32623716dd405cace5154436bb6d
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-10-12 12:29:36 -0300

    main project file

--- src/server.js
+++ src/server.js
@@ -12,5 +12,5 @@ http.createServer(app1).listen(3000);
 console.log('API Documentation 👉', 'Server is up at http://localhost:3000/doc');
 
 app.listen(3333, () => {
-  console.log('API 👉', 'Server is up at http://localhost:3333/transactions');
+  console.log('API 👉', 'Server is up at http://localhost:3333/cashier');
 });
